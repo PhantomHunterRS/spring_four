2252f238f83db4637dd903f06fff8f0836be27e1
phantomhunter <ASerFilyakov@mail.ru>
2022-04-06 00:33:08 +0300

    Initial commit

---
+++ src/main/java/com/phantom/spring_four/SpringFourApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.spring_four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phantom/spring_four/SpringFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom.spring_four;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
